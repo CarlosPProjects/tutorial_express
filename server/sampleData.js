c63e70eaa43bcf37b0417f89851432a873f24046
cg9822@gmail.com <cg9822@gmail.com>
2023-02-12 14:03:58 +0100

    Projects & Clients queries

---
+++ server/sampleData.js
@@ -0,0 +1,77 @@
+const projects = [
+  {
+    id: "1",
+    clientId: "1",
+    name: "eCommerce Website",
+    description:
+      "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500",
+    status: "In Progress",
+  },
+  {
+    id: "2",
+    clientId: "2",
+    name: "React Project",
+    description:
+      "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500",
+    status: "In Progress",
+  },
+  {
+    id: "3",
+    clientId: "3",
+    name: "Diverfy",
+    description:
+      "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500",
+    status: "In Progress",
+  },
+  {
+    id: "4",
+    clientId: "4",
+    name: "Oils Good",
+    description:
+      "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500",
+    status: "In Progress",
+  },
+  {
+    id: "5",
+    clientId: "5",
+    name: "Second Life",
+    description:
+      "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500",
+    status: "In Progress",
+  },
+];
+
+const clients = [
+  {
+    id: "1",
+    name: "Tony Stark",
+    email: "ironman@gmail.com",
+    phone: "343-567-4333",
+  },
+  {
+    id: "2",
+    name: "Tony Mark",
+    email: "ironman@gmail.com",
+    phone: "343-567-4333",
+  },
+  {
+    id: "3",
+    name: "Stilo Merc",
+    email: "ironman@gmail.com",
+    phone: "343-567-4333",
+  },
+  {
+    id: "4",
+    name: "Aurelio S",
+    email: "ironman@gmail.com",
+    phone: "343-567-4333",
+  },
+  {
+    id: "5",
+    name: "Refin York",
+    email: "ironman@gmail.com",
+    phone: "343-567-4333",
+  },
+];
+
+module.exports = { projects, clients };
